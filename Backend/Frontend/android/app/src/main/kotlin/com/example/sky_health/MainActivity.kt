780fc9643af790f09b3e0f729f9ee30172703458
ishatapekar <ishatapekar@gmail.com>
2026-03-31 16:02:38 +0530

    Removed Backend folder

--- Backend/Frontend/android/app/src/main/kotlin/com/example/sky_health/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.sky_health
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Backend/src/routes/canteenRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCanteenMenu } = require('../controllers/canteenController');
-const { protect } = require('../middleware/auth');
-
-router.get('/menu', protect, getCanteenMenu);
-
-module.exports = router;
